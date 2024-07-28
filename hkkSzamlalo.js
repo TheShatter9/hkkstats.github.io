9bc61ad26a2f4f162f513796241879d907203911
Jermi Csaba Márton <94181587+TheShatter9@users.noreply.github.com>
2024-07-28 22:57:21 +0200

    Add files via upload
    kocka pörög

--- hkkSzamlalo.js
+++ hkkSzamlalo.js
@@ -112,10 +112,13 @@ document.getElementById("kockaEldobasa").addEventListener("click", function () {
     const randomSzam = Math.floor(Math.random() * 6) + 1;
     const kockaKep = document.getElementById("kockakep");
     kockaKep.src = `${randomSzam}.png`;
-
+    kockaKep.classList.remove("kockaPorog");
     setTimeout(() => {
         kockaKep.src = `${randomSzam}.png`
     }, 300);
-    kockaKep.classList.remove("kockaPorog");
+
+    void kockaKep.offsetWidth;
+
     kockaKep.classList.add("kockaPorog");
+
 })
