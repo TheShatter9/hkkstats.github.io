adca68f3f2d3a84fd1538602ce8735de09b3c4cc
Jermi Csaba Márton <94181587+TheShatter9@users.noreply.github.com>
2024-07-29 14:35:51 +0200

    Add files via upload
    uj kocka

--- hkkSzamlalo.js
+++ hkkSzamlalo.js
@@ -108,7 +108,7 @@ document.querySelector("#csontMinusz").addEventListener("click", () => {
 });
 
 //Kocka megjelenítése
-document.getElementById("kockaEldobasa").addEventListener("click", function () {
+document.getElementById("kockaAblakMegnyitas").addEventListener("click", function () {
     const randomSzam = Math.floor(Math.random() * 6) + 1;
     const kockaKep = document.getElementById("kockakep");
     kockaKep.src = `${randomSzam}.png`;
